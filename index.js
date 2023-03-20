f22efb0fa1291e541e81fb85c539759b0feb0342
matthew rendely <matthew.rendely@gmail.com>
2023-03-19 18:43:56 -0700

    working with parallel levels, but think there's a better method with styles instead

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ data = {
     second: {
       third: 'tada'
     }
+  },
+  secondouter: {
+    thirdinner: 'tadada'
   }
 }
 
-function addDiv(data){
+function addDiv(data) {
   const el = document.createElement('div');
+  if (typeof data !== 'object') return 'done';
   const key = Object.keys(data)[0];
   if (key === undefined) return 'no key';
   el.innerText = key;
   this.appendChild(el);
-  el.addEventListener('click', function(e) { 
+  el.addEventListener('click', function (e) {
     //Stop propogation so we don't also affect div above
     e.stopPropagation();
     addDiv.call(el, data[key]);
-    console.log('click');
   });
 }
 
-addDiv.call(document.body, data);
+Object.keys(data).forEach(d => {
+  const obj = {}
+  obj[d] = data[d];
+  addDiv.call(document.body, obj);
+})
+
+//TODOs
+//easier approach is to build it entirely but then just hide it with css? 
