e6a5f8cb89e32a63423c73fe44a42db99dcea482
matthew rendely <matthew.rendely@gmail.com>
2023-03-24 11:48:17 -0700

    clean up code and comments

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+// Fetch the reddit JSON data and populate DOM
 fetch("https://www.reddit.com/r/news.json")
-.then(r => r.json())
-.then(allData => {
-//only take list of reddit posts
-const data = allData.data.children;
-Object.keys(data).forEach(d => {
-  const obj = {}
-  obj[d] = data[d];
-  addDiv.call(document.querySelector('main'), obj);
-})
-})
+  .then(r => r.json())
+  .then(allData => {
+    //only take list of reddit posts
+    const data = allData.data.children;
+    Object.keys(data).forEach(d => {
+      const obj = {}
+      obj[d] = data[d];
+      addDiv.call(document.querySelector('main'), obj);
+    })
+  })
 
+// Function for populating DOM based on object data
 function addDiv(data) {
   if (typeof data !== 'object') {
     this.innerText += ` ${JSON.stringify(data)}`;
@@ -20,50 +22,45 @@ function addDiv(data) {
   for (const key of keys) {
     const el = document.createElement('div');
     const val = (typeof data[key] === 'object' ? (Array.isArray(data[key]) ? `Array: ${data[key].length}` : (data[key] === null ? 'null' : 'object')) : String(data[key]))
-    el.innerHTML = `<item-key>${key}:</item-key> <item-value> ${val.slice(0,100)}</item-value>`;
-    // if (!!val.match('\.gif|.jpg|.jpeg|.png')) el.innerHTML += `<img src="${val}" width="200" height="200"/>`
-    if (typeof data[key] === 'object') el.setAttribute('object','');
-    if (data[key] === null) el.setAttribute('null','');
+    el.innerHTML = `<item-key>${key}:</item-key> <item-value> ${val.slice(0, 100)}</item-value>`;
+    if (typeof data[key] === 'object') el.setAttribute('object', '');
+    if (data[key] === null) el.setAttribute('null', '');
     this.appendChild(el);
     if (typeof data[key] === 'object') addDiv.call(el, data[key]);
-    
+
+    //Event listener to expand/collapse an object
     el.addEventListener('click', function toggleShow(e) {
       //Stop propogation so we don't also affect div above
       e.stopPropagation();
-      Array.from(el.children).filter(e=> e.nodeName === 'DIV').forEach(c =>{
-        c.classList.contains('hidden') ? c.classList.remove('hidden') : c.classList.add('hidden')        
+      Array.from(el.children).filter(e => e.nodeName === 'DIV').forEach(c => {
+        c.classList.contains('hidden') ? c.classList.remove('hidden') : c.classList.add('hidden')
       })
-      // el.removeEventListener('click', show);
     });
   }
 }
 
-function filterKeep(string){
+// Function for filtering the DOM based on a keyword
+function filterKeep(string) {
   allDivs = document.querySelectorAll('div');
   allDivs.forEach(d => {
-    // const parentText = Array.from(d.childNodes)
-    // .filter(node => node.nodeType === Node.TEXT_NODE)
-    // .map(node => node.textContent.trim())
-    // .join(" ");
-    const parentText = d.textContent; 
-    if (!parentText.match(string)){
+    const parentText = d.textContent;
+    if (!parentText.match(string)) {
       d.classList.add('hidden');
     }
   })
 }
 
-function unfilter(){
+// Function to unfilter all
+function unfilter() {
   allDivs = document.querySelectorAll('div');
   allDivs.forEach(d => {
-          d.classList.remove('hidden');
-    }
+    d.classList.remove('hidden');
+  }
   )
 }
 
-document.querySelector('#filter').addEventListener('input', (e)=>{
+// Event listener to trigger filtering
+document.querySelector('#filter').addEventListener('input', (e) => {
   unfilter();
   filterKeep(e.target.value);
 })
-
-//TODO:
-//Change it to show it all expanded by default and then click to toggle hiding on/off
